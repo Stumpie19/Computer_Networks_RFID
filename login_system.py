1de8b7dbe5f100c203c8f66b4dfd2f368289df53
Stumpie19 <118705611+Stumpie19@users.noreply.github.com>
2023-03-30 16:40:05 -0400

    Added switch statement

--- login_system.py
+++ login_system.py
@@ -1,33 +1,34 @@
 #Login system to access RFID Timestamp Information
-admin_username = "jgs71"
+admin_username = "admin"
 admin_password = "computernetworks"
 
-print("Welcome...")
-welcome = input("Do you have an account? y/n: ")
-if welcome == "n":
-    while True:
-        alogin = input("Enter admin username: ")
-        apassword = input("Enter admin password: ")
-        if alogin == admin_username and apassword == admin_password:
-            username  = input("Enter a username:")
-            password  = input("Enter a password:")
-            password1 = input("Confirm password:")
-            if password == password1:
-                file = open("master_data.txt", "w")
-                file.write(username+":"+password)
-                file.close()
-                welcome = "y"
-                break
-            print("Passwords do NOT match!")
+print("Welcome!")
+welcome = input("To create an account: 1\nTo login: 2\n")
+
+match welcome:
+    case "1":
+        while True:
+            alogin = input("Enter admin username: ")
+            apassword = input("Enter admin password: ")
+            if alogin == admin_username and apassword == admin_password:
+                username  = input("Enter a username:")
+                password  = input("Enter a password:")
+                password1 = input("Confirm password:")
+                if password == password1:
+                    file = open("master_data.txt", "w")
+                    file.write(username+":"+password)
+                    file.close()
+                    break
+                print("Passwords do NOT match!")
  
-if welcome == "y":
-    while True:
-        login1 = input("Login:")
-        login2 = input("Password:")
-        file = open("master_data.txt", "r")
-        data   = file.readline()
-        file.close()
-        if data == login1+":"+login2:
-            print("Welcome")
-            break
-        print("Incorrect username or password.")
+    case "2":
+        while True:
+            login1 = input("Login:")
+            login2 = input("Password:")
+            file = open("master_data.txt", "r")
+            data   = file.readline()
+            file.close()
+            if data == login1+":"+login2:
+                print("Welcome")
+                break
+            print("Incorrect username or password.")
