0ca362c3fab6a70d773626e44063ddfdca30d1af
Stumpie19 <118705611+Stumpie19@users.noreply.github.com>
2023-04-11 15:59:54 -0400

    Combined LCD and database capabilities

--- database.py
+++ database.py
@@ -24,15 +24,49 @@ def add_data(uid,name,enter,timestamp):
 	except database.Error as e:
 		print(f"Error adding entry to database: {e}")
 
-def get_data(name):
+def get_datauid(uid):
 	try:
-		statement = "SELECT UNIQUE ID WHOSE NAME MATCHES ID#:%c"
-		data = (uid)
+		statement = "SELECT uid FROM database WHERE uid=%c"
+		data = (uid,)
 		cursor.execute(statement, data)
 		for (uid) in cursor:
+			print(f"Successfully retrieved {uid}")
+	except database.Error as e:
+		print(f"Error retrieving entry from database: {e}")
+	return uid
+
+def get_dataname(uid):
+	try:
+		statement = "SELECT name FROM database WHERE uid=%c"
+		data = (uid,)
+		cursor.execute(statement, data)
+		for (uid, name) in cursor:
 			print(f"Successfully retrieved {uid}, {name}")
 	except database.Error as e:
 		print(f"Error retrieving entry from database: {e}")
+	return name
+
+def get_dataenter(uid):
+	try:
+		statement = "SELECT enter FROM database WHERE uid=%c"
+		data = (uid,)
+		cursor.execute(statement, data)
+		for (uid, enter) in cursor:
+			print(f"Successfully retrieved {uid},{enter}")
+	except database.Error as e:
+		print(f"Error retrieving entry from database: {e}")
+	return enter
+
+def get_data(uid):
+	try:
+		statement = "SELECT uid, name, enter FROM database WHERE uid=%c"
+		data = (uid,)
+		cursor.execute(statement, data)
+		for (uid, name, enter) in cursor:
+			print(f"Successfully retrieved {uid}, {name}, {enter}")
+	except database.Error as e:
+		print(f"Error retrieving entry from database: {e}")
+
 
 add_data("123456789012", "Jacob", 1, localtime)
 get_data("123456789012")
--- lcd.py
+++ lcd.py
@@ -1,16 +1,33 @@
 
 from signal import signal, SIGTERM, SIGHUP, pause
+from database import get_dataname, get_dataenter
 
 #From raspberry pi lcd library
 from rpi_lcd import LCD
+uid = "1"
+get_data(uid)
+
 lcd = LCD()
 def safe_exit(signum, frame):
     exit(1)
 try:
     signal(SIGTERM, safe_exit)
     signal(SIGHUP, safe_exit)
-    lcd.text("Hello,", 1)
-    lcd.text("Raspberry Pi!", 2)
+    get_data()
+    name1 = get_dataname()
+    enter = get_dataenter
+    if enter == 1:
+
+        lcd.text("Welcome,", 1)
+
+        #Get name of attendee from database.py
+        lcd.text(name1, 2)
+    
+    else
+        lcd.text("Good-Bye", 1)
+
+        #Get name of attendee from database.py
+        lcd.text(name1, 2)
     pause()
 except KeyboardInterrupt:
     pass
