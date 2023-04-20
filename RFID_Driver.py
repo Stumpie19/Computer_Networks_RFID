2ec00edd152d6b4c074b0db7dede98a988471c8b
aas183 <93923758+aas183@users.noreply.github.com>
2023-04-19 23:19:39 -0400

    RFID 4/19/23

---
+++ RFID_Driver.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+
+from . import RFID_Reader
+import RPi.GPIO as GPIO
+  
+class RFID_READER:
+
+  READER = None
+  
+  #KEY = [0xFF,0xFF,0xFF,0xFF,0xFF,0xFF]
+  #BLOCK_ADDRS = [8, 9, 10]
+  
+  def __init__(self):
+    self.READER = RFID()
+    
+  #get uid of PICC
+  def get_id(self):
+    id = self.read_uid()
+    while not id:
+      id = self.read_uid()
+    return id
+
+  #read uid of PICC
+  def read_uid(self):
+      (status, TagType) = self.READER.requestPICC()#Send request command to PICC to move them from idle to ready state
+      if status != self.READER.STATUS_OK:#Error Check
+          return None
+      
+      (status, uid) = self.READER.anticollisionPICC()#Perform Anticollision action to get uid of PICC
+      if status != self.READER.STATUS_OK:#Error Check 
+          return None
+      
+      return self.uid_to_decimal(uid)#Convert UID to decimal and return it
+  
+  #Convert unqiue ID of PICC from hexidecimal to decimal
+  def uid_to_decimal(self, uid):
+      n = 0
+      for i in range(0, 5):#loop through uid hexidecimal
+          n = n * 256 + uid[i]#convert hexidecimal to uid
+      return n
