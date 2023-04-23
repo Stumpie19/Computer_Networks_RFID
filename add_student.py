d2e47935452ce5aa1db2242d92f8981712c26f01
Jason Lyon <jl345@uakron.edu>
2023-04-23 14:46:14 -0400

    Add python scripts for adding and deleting students

---
+++ add_student.py
@@ -0,0 +1,18 @@
+from database import add_data
+import RFID_Driver
+import RPi.GPIO as GPIO
+import time
+
+
+print("Scan RFID tag:")
+
+reader = RFID_Driver.RFID_READER()
+try:
+    uid = reader.get_id()
+    print(uid)
+finally:
+    GPIO.cleanup()
+print()
+
+name = input("Enter name:")
+add_data(uid, name, 0, time.time())
