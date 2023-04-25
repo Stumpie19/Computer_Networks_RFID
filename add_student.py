5cdb5effadab7687af523ecabd9330906a799430
Jason Lyon <jl345@uakron.edu>
2023-04-25 17:26:31 -0400

    Add comments to add_student and delete_student code

--- add_student.py
+++ add_student.py
@@ -5,31 +5,44 @@ import time
 from signal import signal, SIGTERM, SIGHUP, pause
 from rpi_lcd import LCD
 
+#Initialize LCD
 lcd = LCD()
 def safe_exit(signum, frame):
     exit(1)
 
 signal(SIGTERM, safe_exit)
 signal(SIGHUP, safe_exit)
+
+#Display "Scan RFID tag" on LCD
 lcd.text("Scan RFID tag", 1)
 
 print("Scan RFID tag:")
 
+#Initialize RFID Reader/Driver
 reader = RFID_Driver.RFID_READER()
+
 try:
+    #Fetch RFID
     uid = reader.get_id()
 finally:
     GPIO.cleanup()
 
 lcd.clear()
+
+#Display "Enter name" on LCD
 lcd.text("Enter name", 1)
+
 name = input("Enter name:")
+
+#Add an entry to database
 result = add_data(uid, name, 0, int(time.time()), int(time.time()))
 
 if result == 0:
     print(f"Successfully added {name} to database")
     
     lcd.clear()
+
+    #Display confirmation message on LCD
     lcd.text(str(name), 1)
     lcd.text("added to DB", 2)
 
@@ -37,9 +50,13 @@ else:
     print("Error adding student to database")
     
     lcd.clear()
+
+    #Display error message on LCD
     lcd.text("Error", 1)
 
+#Wait 5 seconds before clearing LCD
 time.sleep(5)
 lcd.clear()
+
 signal(SIGTERM, safe_exit)
 signal(SIGHUP, safe_exit)
--- delete_student.py
+++ delete_student.py
@@ -4,30 +4,43 @@ import mysql.connector as database
 
 while True:
     try:
+        #Prompt user for database login credentials
         username = input("Username:")
         password = input("Password:")
+        
+        #Connect to MySQL Database
         connection = database.connect(
             user=username,
             password=password,
             host="localhost",
             database="CNattendance")
 
+        #Create a cursor
         cursor = connection.cursor()
 
         print("Scan RFID tag:")
+        
+        #Initialize RFID Reader/Driver
         reader = RFID_Driver.RFID_READER()
+        
         try:
+            #Fetch RFID
             uid = reader.get_id()
         finally:
             GPIO.cleanup()
 
         try:
             statement = f"DELETE FROM attendance WHERE uid = '{uid}'"
+            
+            #Execute above statement
             cursor.execute(statement)
             connection.commit()
+            
             print(f"Student successfully removed from database")
+
         except database.Error as e:
             print(f"Error deleting student from database: {e}")
+            
         finally:
             connection.close()
             break
