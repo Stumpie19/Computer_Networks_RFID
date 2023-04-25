583128be6454b0e7f0efb8a912493b3a6fe20fee
Stumpie19 <118705611+Stumpie19@users.noreply.github.com>
2023-04-25 15:27:54 -0400

    Removed unecessary file and added comments

--- attendance.py
+++ attendance.py
@@ -9,7 +9,7 @@ from timestamp import localtime
 #From raspberry pi lcd library
 from rpi_lcd import LCD
 
-
+#Initialize LCD
 lcd = LCD()
 def safe_exit(signum, frame):
     exit(1)
@@ -18,32 +18,43 @@ try:
         signal(SIGTERM, safe_exit)
         signal(SIGHUP, safe_exit)
 
+        #Initialize RFID Reader/Driver
         reader = RFID_Driver.RFID_READER()
 
         try:
+            #Fetch RFID
             uid = reader.get_id()
             print("\nuid = "+str(uid))
         finally:
             GPIO.cleanup()
 
+        #Get name and in/out values from MySQL database
         name = get_dataname(uid)
         enter = get_dataenter(uid)
+
+        #Student enters room
         if enter == 0:
             update_timestamp_in(uid, int(time.time()))
             print("Welcome "+str(name))
+
+            #Display welcome on first line of LCD
             lcd.text("Welcome,", 1)
 
             #Get name of attendee from database.py
             lcd.text(str(name), 2)
         
+        #Student leaves room
         elif enter == 1:
             update_timestamp_out(uid, int(time.time()))
             print("Good-Bye "+str(name))
+
+            #Display good-bye on first line of LCD
             lcd.text("Good-Bye", 1)
 
             #Get name of attendee from database.py
             lcd.text(str(name), 2)
         else:
+            #Throw error
             lcd.text("Error", 1)
             lcd.text(str(name), 2)
         time.sleep(2)
