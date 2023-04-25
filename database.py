a0c7018ef27382e72132c3b60d25ac51953bda67
Stumpie19 <118705611+Stumpie19@users.noreply.github.com>
2023-04-25 15:26:44 -0400

    Removed unecessary file and added comments

--- database.py
+++ database.py
@@ -1,22 +1,26 @@
 import os
 import mysql.connector as database
 
-from timestamp import localtime
-
-
+#Connect to MySQL Database
 connection = database.connect(
     user="CNuser",
     password="CNpassword",
     host="localhost",
     database="CNattendance")
 
+#Create a cursor
 cursor = connection.cursor()
 
+#Adding entry into database
 def add_data(uid,name,enter,timestamp_in, timestamp_out):
     try:
         statement = "INSERT INTO attendance (uid, name, enter, timestamp_in, timestamp_out) VALUES ('"+str(uid)+"','"+str(name)+"',"+str(enter)+",'"+str(timestamp_in)+"','"+str(timestamp_out)+"')"
+
+        #Execute above statement
         cursor.execute(statement)
+        #Commit to database
         connection.commit()
+
         print("uid = "+str(uid))
         print("Successfully added entry to database")
         return 0
@@ -24,10 +28,14 @@ def add_data(uid,name,enter,timestamp_in, timestamp_out):
         print(f"Error adding entry to database: {e}")
         return -1
 
+#Pulling name from database
 def get_dataname(uid):
     try:
         statement = "Select name FROM attendance WHERE uid="+str(uid)
+
+        #Execute above statement
         cursor.execute(statement)
+        #Fetching a row in the MySQL database
         name = cursor.fetchone()
 
         if cursor.rowcount < 1:
@@ -39,11 +47,15 @@ def get_dataname(uid):
         print(f"Error retrieving entry from database: {e}")
         return -1
     
+#Pulling in/out from database
 def get_dataenter(uid):
     try:
 
         statement = "SELECT enter FROM attendance WHERE uid="+str(uid)
+
+        #Execute above statement        
         cursor.execute(statement)
+        #Fetch a row in the MySQL Database
         enter = cursor.fetchone()
 
         if cursor.rowcount < 1:
@@ -53,7 +65,10 @@ def get_dataenter(uid):
             statement = "UPDATE attendance SET enter=0 WHERE uid="+str(uid)
         else:
             statement = "UPDATE attendance SET enter=1 WHERE uid="+str(uid)
+        
+        #Execute above statement
         cursor.execute(statement)
+        #Commit changes
         connection.commit()
 
         return enter[0]
@@ -62,31 +77,47 @@ def get_dataenter(uid):
         print(f"Error retrieving entry from database: {e}")
         return -1
 
+#Deleting entry from database
 def delete_data(uid):
     try:
         statement = f"DELETE FROM attendance WHERE uid = '{uid}'"
+
+        #Execute above statement
         cursor.execute(statement)
+        #Commit to database
         connection.commit()
+
         print("uid = "+str(uid))
         print("Successfully deleted entry to database")
     except database.Error as e:
         print(f"Error deleting entry from database: {e}")
 
+#Update time in
 def update_timestamp_in(uid, timestamp_in):
     try:
         statement = f"UPDATE attendance SET timestamp_in = '{timestamp_in}' WHERE uid = '{uid}'"
+
+        #Execute above statement
         cursor.execute(statement)
+        #Commit to database
         connection.commit()
+
     except database.Error as e:
         print(f"Error updating timestamp in database: {e}")
 
+#Update time out
 def update_timestamp_out(uid, timestamp_out):
     try:
         statement = f"UPDATE attendance SET timestamp_out = '{timestamp_out}' WHERE uid = '{uid}'"
+
+        #Execute above statement
         cursor.execute(statement)
+        #Commit to database
         connection.commit()
+
     except database.Error as e:
         print(f"Error updating timestamp in database: {e}")
 
+#Close database
 def close_database():
     connection.close()
